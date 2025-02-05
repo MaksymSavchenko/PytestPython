52d02749bf5be804be8e5bbfaf57e9acfdbc62e9
max <maxsavchenko@gmail.com>
2025-02-04 22:21:40 -0800

    Added strings, child, plementation

---
+++ Strings.py
@@ -0,0 +1,14 @@
+str = "google.com"
+str1 = 'website'
+str3 = 'com'
+str4 = " good morning "
+
+print(str[1])
+print(str[0:6])
+print(str+' '+str1) #concatenation
+print(str3 in str)  #substring check
+print(str.split('.'))
+print(str.split('.')[0])
+print(str4.strip())
+print(str4.lstrip())
+print(str4.rstrip())
