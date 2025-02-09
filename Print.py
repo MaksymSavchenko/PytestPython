52d7ea87b6d8914d86d65b421830bc6e7400ddc8
max <maxsavchenko@gmail.com>
2025-02-08 22:03:34 -0800

    Added write file, pyTestValidation

--- Print.py
+++ Print.py
@@ -6,8 +6,8 @@ print(Str)
 
 b, c, d = 2, 5.5, "Max"
 print(d)
-#print("Value is" +c) Will not work
 
+#print("Value is" +c) Will not work
 print("{} {}".format("Value is", c))
 print(type(b))
 print(type(c))
