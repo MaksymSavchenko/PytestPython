ab086767cafbf9d87db54cbdc4775a0406a0ca36
max <maxsavchenko@gmail.com>
2025-02-12 20:42:20 -0800

    Added basic locators

--- playwright/test_PlaywrightBasics.py
+++ playwright/test_PlaywrightBasics.py
@@ -1,6 +1,6 @@
 import time
 
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page, Playwright, expect
 
 
 def test_playwrightBasics(playwright):
@@ -16,7 +16,7 @@ def test_playwrightShortCut(page:Page): #page - fixture; Page - class
 def test_core_locators(page:Page):
     page.goto("https://rahulshettyacademy.com/loginpagePractise")
     page.get_by_label("Username").fill("rahulshettyacademy")
-    page.get_by_label("Password").fill("learning1")
+    page.get_by_label("Password").fill("learning1") #incorrect password
     page.get_by_role("combobox").select_option("consult")
     page.get_by_role("link", name="terms and conditions").click()
 #    page.get_by_role("checkbox", name="terms").click()
@@ -25,4 +25,22 @@ def test_core_locators(page:Page):
     # Incorrect  username/password. assertion
     expect(page.get_by_text("Incorrect username/password")).to_be_visible()
 
-    time.sleep(5)
+
+def test_firefox(playwright:Playwright):
+    firefox_browser = playwright.firefox.launch(headless=False)
+    # context = firefox_browser.new_context() #new set of cookies, cache Not needed for just 1 page
+    page = firefox_browser.new_page()
+    page.goto("https://rahulshettyacademy.com/loginpagePractise")
+    page.get_by_label("Username").fill("rahulshettyacademy")
+    page.get_by_label("Password").fill("learning1") #incorrect password
+    page.get_by_role("combobox").select_option("consult")
+    page.get_by_role("link", name="terms and conditions").click()
+#    page.get_by_role("checkbox", name="terms").click()
+    page.locator("#terms").check()
+    page.get_by_role("button", name="Sign In").click()
+    # Incorrect  username/password. assertion
+    expect(page.get_by_text("Incorrect username/password")).to_be_visible()
+
+
+
+time.sleep(5)
