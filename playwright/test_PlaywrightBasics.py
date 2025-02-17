c7062a3ec927c4f884de6be1ab1cfc788fb5e346
Max <maxsavchenko@gmail.com>
2025-02-16 18:45:53 -0800

    Merge pull request #6 from MaksymSavchenko/master
    tests have been added

--- playwright/test_PlaywrightBasics.py
+++ playwright/test_PlaywrightBasics.py
@@ -1,7 +1,5 @@
 import time
-
 from playwright.sync_api import Page, Playwright, expect
-
 
 def test_playwrightBasics(playwright):
     browser = playwright.chromium.launch(headless=False)
@@ -25,7 +23,6 @@ def test_core_locators(page:Page):
     # Incorrect  username/password assertion
     expect(page.get_by_text("Incorrect username/password")).to_be_visible()
 
-
 def test_firefox(playwright:Playwright):
     firefox_browser = playwright.firefox.launch(headless=False)
     # context = firefox_browser.new_context() #new set of cookies, cache Not needed for just 1 page
@@ -47,7 +44,9 @@ def test_child_window(page:Page):
         page.locator(".blinkingText").click()
         child_page = new_page.value
         text = child_page.locator(".red").text_content()
-        print('\n' + text.split(' ')[4])
+        email = text.split(' ')[4].strip() #strip (removing spaces before and after) not needed.
+        print('\n' + email)
+        assert email == "mentor@rahulshettyacademy.com"
 
 
 time.sleep(5)
