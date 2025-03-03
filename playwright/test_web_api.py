50cfba423968ac211f47417eefbabc4e1213b21a
max <maxsavchenko@gmail.com>
2025-03-02 20:10:41 -0800

    Added hybrid test API tests

--- playwright/test_web_api.py
+++ playwright/test_web_api.py
@@ -1,13 +1,19 @@
 import time
 from playwright.sync_api import Page, expect, Playwright
 
+from utils.api_base import APIutils
+
 
 def test_e2e_web_api(playwright:Playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
+
+    api_utils = APIutils()
+    api_utils.create_order(playwright)
+
     page.goto("https://rahulshettyacademy.com/client")
     page.get_by_placeholder("email@example.com").fill("rahulshetty@gmail.com")
-    page.get_by_placeholder("enter your password").fill("Iamking@000")
+    page.get_by_placeholder("enter your passsword").fill("Iamking@000")
     page.get_by_role("button", name="Login").click()
 
