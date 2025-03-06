39254f787daa289e2b420e213a019dcc36efd526
max <maxsavchenko@gmail.com>
2025-03-05 21:56:10 -0800

    Fixed hybrid test API tests

--- playwright/utils/api_base.py
+++ playwright/utils/api_base.py
@@ -1,12 +1,12 @@
 from playwright.sync_api import Playwright
 credentials = {"userEmail":"rahulshetty@gmail.com","userPassword":"Iamking@000"}
-orders_payload = {"orders":[{"country":"Ukraine","productOrderedId":"6581ca399fd99c85e8ee7f45"}]}
+orders_payload = {"orders":[{"country":"Ukraine","productOrderedId":"67a8df1ac0d3e6622a297ccb"}]}
 
 class APIutils:
 
     def get_token(self,playwright: Playwright):
         api_request_context = playwright.request.new_context(base_url="https://rahulshettyacademy.com")
-        response = api_request_context.post("/api/ecom/auth/login", data=credentials)
+        response = api_request_context.post("/api/ecom/auth/login", data = credentials)
         assert response.ok
         print(response.json())
         response_body = response.json()
@@ -16,8 +16,7 @@ class APIutils:
         token=self.get_token(playwright)
         api_request_context = playwright.request.new_context(base_url="https://rahulshettyacademy.com")
         response = api_request_context.post("/api/ecom/order/create-order",
-                                 data= orders_payload,
-                                 headers={"Authorization": token,
-                                          "Content-type:": "application/json"
-                                          })
+                                            headers = {"Authorization": token, "Content-type": "application/json"},
+                                            data=orders_payload
+        )
         print(response.json())
