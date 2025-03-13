9324cc7bc5a2d23e76d2ee314cc6c379f3e39323
Max <maxsavchenko@gmail.com>
2025-03-12 22:00:56 -0700

    Merge pull request #6 from MaksymSavchenko/master
    tests have been added

--- playwright/test_web_api.py
+++ playwright/test_web_api.py
@@ -9,10 +9,15 @@ def test_e2e_web_api(playwright: Playwright):
     page = context.new_page()
 
     api_utils = APIutils()
-    api_utils.create_order(playwright)
+    order_id = api_utils.create_order(playwright)
 
     page.goto("https://rahulshettyacademy.com/client")
     page.get_by_placeholder("email@example.com").fill("rahulshetty@gmail.com")
     page.get_by_placeholder("enter your passsword").fill("Iamking@000")
     page.get_by_role("button", name="Login").click()
+    page.get_by_role("button", name="ORDERS").click()
 
+    row = page.locator("tr").filter(has_text=order_id)
+    row.get_by_role("button", name="View").click()
+    expect(page.locator(".tagline")).to_have_text("Thank you for Shopping With Us")
+    context.close() #optional to clear the cache
